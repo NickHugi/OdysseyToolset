9b9724dc97a5c28226bb31998047e237ab14a8db
cortisol <hugi.nicholas@hotmail.com>
2020-03-02 16:44:14 +0800

    added model viewer

--- widgets/placeable_editor.py
+++ widgets/placeable_editor.py
@@ -3,9 +3,12 @@ from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QBrush, QFontInfo, QFontMetrics
 from PyQt5.QtWidgets import QWidget, QPushButton, QLineEdit, QSpinBox, QCheckBox, QComboBox, QSlider, QTreeWidgetItem, \
     QPlainTextEdit, QFrame, QSizePolicy, QLabel
+from pykotor.formats.mdl import MDL
 
 from installation import Installation
+from pykotor.formats.twoda import TwoDA
 from ui import placeable_editor
+from widgets.model_renderer import ModelRenderer, Object
 from widgets.tree_editor import AbstractTreeEditor
 
 
@@ -17,6 +20,10 @@ class PlaceableEditor(AbstractTreeEditor):
         self.ui.setupUi(self)
 
         self.installation = self.window().active_installation
+
+        if self.installation is not None:
+            self.model_renderer = ModelRenderer(self)
+            self.ui.splitter.addWidget(self.model_renderer)
 
         self.init_tree()
 
@@ -35,9 +42,9 @@ class PlaceableEditor(AbstractTreeEditor):
         self.init_combo_box("Basic", "Body Bag", items=Installation.get_bodybag_list())
 
         self.init_spin_box("Name", "TLK Reference")
+        self.get_node_widget("Name", "TLK Reference").valueChanged.connect(self.tlk_name_changed)
         self.init_line_edit("Name", "TLK Text")
         self.get_node_widget("Name", "TLK Text").setReadOnly(True)
-        self.get_node_widget("Name", "TLK Reference").valueChanged.connect(self.tlk_name_changed)
         self.init_line_edit("Name", "English")
         self.init_line_edit("Name", "French")
         self.init_line_edit("Name", "German")
@@ -99,7 +106,8 @@ class PlaceableEditor(AbstractTreeEditor):
             self.get_node("Name", "TLK Text").parent().removeChild(self.get_node("Name", "TLK Text"))
             self.init_spin_box("Basic", "Appearance")
         else:
-            self.init_combo_box("Basic", "Appearance", items=Installation.get_door_list(self.installation))
+            self.init_combo_box("Basic", "Appearance", items=Installation.get_placeable_list(self.installation))
+            self.get_node_widget("Basic", "Appearance").currentIndexChanged.connect(self.appearance_changed)
 
     def open_inventory_dialog(self):
         # TODO
@@ -111,4 +119,17 @@ class PlaceableEditor(AbstractTreeEditor):
             if index < self.installation.get_tlk_entry_count():
                 text = self.installation.get_tlk_entry_text(index)
                 self.get_node_widget("Name", "TLK Text").setText(text)
+
+    def appearance_changed(self, index):
+        try:
+            genericdoors_data = TwoDA.from_data(self.installation.chitin.fetch_resource("placeables", "2da"))
+            model_name = genericdoors_data.get_cell("modelname", index).lower()
+            mdl_data = self.installation.chitin.fetch_resource(model_name, "mdl")
+            mdx_data = self.installation.chitin.fetch_resource(model_name, "mdx")
+            model = MDL.from_data(mdl_data, mdx_data)
+            self.model_renderer.model_buffer[model_name] = model
+            self.model_renderer.objects.clear()
+            self.model_renderer.objects.append(Object(model_name))
+        except Exception as e:
+            print("Failed to load door appearance model:", e)
 
