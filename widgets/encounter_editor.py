8926a87d9b417066a80275943dc882f16e2da412
cortisol <hugi.nicholas@hotmail.com>
2020-03-05 00:51:57 +0800

    clean up and partial .ute loading support

--- widgets/encounter_editor.py
+++ widgets/encounter_editor.py
@@ -3,10 +3,12 @@ from PyQt5.QtGui import QStandardItemModel, QStandardItem, QBrush
 from PyQt5.QtWidgets import QWidget, QTreeWidgetItem, QPushButton, QPlainTextEdit, QTreeWidget, QLineEdit, QSpinBox, \
     QCheckBox, QComboBox
 
+from installation import Installation
 from ui import encounter_editor
+from widgets.tree_editor import AbstractTreeEditor
 
 
-class EncounterEditor(QWidget):
+class EncounterEditor(AbstractTreeEditor):
     def __init__(self, parent):
         QWidget.__init__(self, parent)
 
@@ -15,134 +17,63 @@ class EncounterEditor(QWidget):
 
         self.installation = self.window().active_installation
 
-        search_flags = QtCore.Qt.MatchExactly | QtCore.Qt.MatchRecursive
-
-        self.creatures_item = self.ui.tree.findItems("Creatures", search_flags)[0]
-        self.script_tag_item = self.ui.tree.findItems("Script Tag", search_flags)[0]
-        self.template_item = self.ui.tree.findItems("Template", search_flags)[0]
-        self.difficulty_item = self.ui.tree.findItems("Difficulty", search_flags)[0]
-        self.faction_item = self.ui.tree.findItems("Faction", search_flags)[0]
-        self.min_creatures_item = self.ui.tree.findItems("Min Creatures", search_flags)[0]
-        self.max_creatures_item = self.ui.tree.findItems("Max Creatures", search_flags)[0]
-        self.interval_item = self.ui.tree.findItems("Interval", search_flags)[0]
-        self.waves_item = self.ui.tree.findItems("Waves", search_flags)[0]
-
-        self.tlk_reference_item = self.ui.tree.findItems("TLK Reference", search_flags)[0]
-        self.tlk_text_item = self.ui.tree.findItems("TLK Text", search_flags)[0]
-        self.english_item = self.ui.tree.findItems("English", search_flags)[0]
-        self.french_item = self.ui.tree.findItems("French", search_flags)[0]
-        self.german_item = self.ui.tree.findItems("German", search_flags)[0]
-        self.italian_item = self.ui.tree.findItems("Italian", search_flags)[0]
-        self.spanish_item = self.ui.tree.findItems("Spanish", search_flags)[0]
-        self.polish_item = self.ui.tree.findItems("Polish", search_flags)[0]
-        self.korean_item = self.ui.tree.findItems("Korean", search_flags)[0]
-
-        self.active_item = self.ui.tree.findItems("Active", search_flags)[0]
-        self.player_activated_item = self.ui.tree.findItems("Player-Activated", search_flags)[0]
-        self.will_reset_item = self.ui.tree.findItems("Will Reset", search_flags)[0]
-        self.one_time_item = self.ui.tree.findItems("One-Time", search_flags)[0]
-
-        self.routine_item = self.ui.tree.findItems("Routine", search_flags)[0]
-        self.entered_item = self.ui.tree.findItems("Entered", search_flags)[0]
-        self.exited_item = self.ui.tree.findItems("Exited", search_flags)[0]
-        self.all_dead_item = self.ui.tree.findItems("All Dead", search_flags)[0]
-        self.custom_item = self.ui.tree.findItems("Custom", search_flags)[0]
-
         self.init_tree()
 
     def init_tree(self):
         for i in range(self.ui.tree.topLevelItemCount()):
             self.ui.tree.topLevelItem(i).setForeground(0, QBrush(QtCore.Qt.gray))
 
-        self.init_button(self.creatures_item, "...")
-        self.ui.tree.itemWidget(self.creatures_item, 1).clicked.connect(self.open_creatures_dialog)
+        creatures_button = QPushButton("...")
+        creatures_button.setFixedHeight(17)
+        creatures_button.clicked.connect(self.open_creatures_dialog)
+        self.ui.tree.setItemWidget(self.ui.tree.findItems("Creatures", QtCore.Qt.MatchExactly)[0], 1, creatures_button)
 
-        self.init_line_edit(self.script_tag_item)
-        self.init_line_edit(self.template_item)
-        self.init_spin_box(self.min_creatures_item)
-        self.init_spin_box(self.max_creatures_item)
-        self.init_spin_box(self.interval_item)
-        self.init_spin_box(self.waves_item)
-        self.init_check_box(self.active_item)
-        self.init_check_box(self.player_activated_item)
-        self.init_check_box(self.will_reset_item)
-        self.init_check_box(self.one_time_item)
+        self.init_line_edit("Basic", "Script Tag")
+        self.init_line_edit("Basic", "Template")
+        self.init_combo_box("Basic", "Faction", Installation.get_faction_list())
+        self.init_combo_box("Basic", "Difficulty", ["Very Easy", "Easy", "Normal", "Hard", "Impossible"])
+        self.init_spin_box("Basic", "Min Creatures")
+        self.init_spin_box("Basic", "Max Creatures")
+        self.init_spin_box("Basic", "Interval")
+        self.init_spin_box("Basic", "Waves")
 
-        self.init_spin_box(self.tlk_reference_item)
-        self.init_line_edit(self.tlk_text_item)
-        self.ui.tree.itemWidget(self.tlk_text_item, 1).setReadOnly(True)
-        self.tlk_text_item.setDisabled(True)
-        self.ui.tree.itemWidget(self.tlk_reference_item, 1).valueChanged.connect(self.tlk_reference_changed)
-        self.init_line_edit(self.english_item)
-        self.init_line_edit(self.french_item)
-        self.init_line_edit(self.german_item)
-        self.init_line_edit(self.italian_item)
-        self.init_line_edit(self.spanish_item)
-        self.init_line_edit(self.polish_item)
-        self.init_line_edit(self.korean_item)
+        self.init_check_box("Flags", "Active")
+        self.init_check_box("Flags", "Player-Activated")
+        self.init_check_box("Flags", "Will Reset")
+        self.init_check_box("Flags", "One-Time")
 
-        self.init_line_edit(self.routine_item)
-        self.init_line_edit(self.entered_item)
-        self.init_line_edit(self.exited_item)
-        self.init_line_edit(self.all_dead_item)
-        self.init_line_edit(self.custom_item)
+        self.init_localized_string_nodes("Name")
 
-        self.init_combo_box(self.faction_item, ["Friendly 1", "Hostile 2", "Friendly 2", "Neutral", "Insane", "Tuskan",
-                                                "GLB XOR", "Surrender 1", "Surrender 2", "Predator", "Prey", "Trap",
-                                                "Endar Spire", "Rancor", "Gizka 1", "Gizka 2", "Czerka",
-                                                "Zone Controller", "Sacrafice", "One On One", "Party Puppet"])
+        self.init_line_edit("Scripting", "Routine")
+        self.init_line_edit("Scripting", "Entered")
+        self.init_line_edit("Scripting", "Exited")
+        self.init_line_edit("Scripting", "All Dead")
+        self.init_line_edit("Scripting", "Custom")
 
-        self.init_combo_box(self.difficulty_item, ["Very Easy", "Easy", "Normal", "Hard", "Impossible"])
-
-        if self.installation is None:
-            self.ui.tree.findItems("Naming", QtCore.Qt.MatchExactly | QtCore.Qt.MatchRecursive)[0].removeChild(self.tlk_text_item)
-    
     def open_creatures_dialog(self):
         # TODO
         print("click")
 
-    def tlk_reference_changed(self, index):
-        self.ui.tree.itemWidget(self.tlk_text_item, 1).setText("")
-        if self.installation is not None:
-            if index < self.installation.get_tlk_entry_count():
-                text = self.installation.get_tlk_entry_text(index)
-                self.ui.tree.itemWidget(self.tlk_text_item, 1).setText(text)
+    def load(self, utw):
+        self.set_note_data("Basic", "Script Tag", utw.find_field_data("Tag", default=""))
+        self.set_note_data("Basic", "Template", utw.find_field_data("TemplateResRef", default=""))
+        self.set_note_data("Basic", "Faction", utw.find_field_data("Faction", default=0))
+        self.set_note_data("Basic", "Difficulty", utw.find_field_data("DifficultyIndex", default=0))
+        self.set_note_data("Basic", "Min Creatures", utw.find_field_data("RecCreatures", default=0))
+        self.set_note_data("Basic", "Max Creatures", utw.find_field_data("MaxCreatures", default=0))
+        self.set_note_data("Basic", "Interval", utw.find_field_data("ResetTime", default=0))
+        self.set_note_data("Basic", "Waves", utw.find_field_data("Reset", default=0))
 
-    def init_button(self, item, text):
-        button = QPushButton()
-        button.setText(text)
-        button.setFixedHeight(17)
-        self.ui.tree.setItemWidget(item, 1, button)
-        return button
+        self.set_note_data("Flags", "Active", utw.find_field_data("Active", default=False))
+        self.set_note_data("Flags", "Player-Activated", utw.find_field_data("PlayerOnly", default=False))
+        self.set_note_data("Flags", "Will Reset", utw.find_field_data("Reset", default=False))
+        self.set_note_data("Flags", "One-Time", utw.find_field_data("SpawnOption", default=False))
 
-    def init_line_edit(self, item):
-        line_edit = QLineEdit()
-        line_edit.setFixedHeight(23)
-        line_edit.setStyleSheet("background: rgb(0,0,0,0%)")
-        self.ui.tree.setItemWidget(item, 1, line_edit)
-        return line_edit
+        self.set_localized_string_nodes("Name", utw.find_field_data("LocalizedName"))
 
-    def init_spin_box(self, item, min=0, max=999999):
-        spin_box = QSpinBox()
-        spin_box.setMaximum(max)
-        spin_box.setMinimum(min)
-        spin_box.setFixedHeight(23)
-        spin_box.setStyleSheet("background: rgb(0,0,0,0%); border-width: 0px; border-style: none;")
-        self.ui.tree.setItemWidget(item, 1, spin_box)
-        return spin_box
+        self.set_note_data("Scripting", "Routine", utw.find_field_data("ScriptHeartbeat", default=""))
+        self.set_note_data("Scripting", "Entered", utw.find_field_data("ScriptOnEnter", default=""))
+        self.set_note_data("Scripting", "Exited", utw.find_field_data("ScriptOnExit", default=""))
+        self.set_note_data("Scripting", "All Dead", utw.find_field_data("OnClick", default=""))
+        self.set_note_data("Scripting", "Custom", utw.find_field_data("ScriptUserDefine", default=""))
 
-    def init_check_box(self, item, checked=False):
-        check_box = QCheckBox()
-        check_box.setFixedHeight(23)
-        check_box.setStyleSheet("QCheckBox::indicator { width: 23; height: 23;}")
-        self.ui.tree.setItemWidget(item, 1, check_box)
-        return check_box
-
-    def init_combo_box(self, item, items=[]):
-        combo_box = QComboBox()
-        combo_box.setFixedHeight(23)
-        combo_box.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        combo_box.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        combo_box.addItems(items)
-        self.ui.tree.setItemWidget(item, 1, combo_box)
-        return combo_box
