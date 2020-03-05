30cdfd899cfb167019d5e1610197bf12ae7d9ad4
cortisol <hugi.nicholas@hotmail.com>
2020-03-05 09:29:37 +0800

    cleaned up and added partial .uti loading support

--- widgets/item_editor.py
+++ widgets/item_editor.py
@@ -29,7 +29,7 @@ class ItemEditor(AbstractTreeEditor):
         for i in range(self.ui.tree.topLevelItemCount()):
             self.ui.tree.topLevelItem(i).setForeground(0, QBrush(QtCore.Qt.gray))
 
-        properties_button = QPushButton("Properties")
+        properties_button = QPushButton("...")
         properties_button.setFixedHeight(17)
         properties_button.clicked.connect(self.open_properties_dialog)
         self.ui.tree.setItemWidget(self.ui.tree.findItems("Properties", QtCore.Qt.MatchExactly)[0], 1, properties_button)
@@ -40,31 +40,9 @@ class ItemEditor(AbstractTreeEditor):
         self.init_spin_box("Basic", "Additional Cost")
         self.init_check_box("Basic", "Plot Item")
 
-        self.init_spin_box("Name", "TLK Reference")
-        self.get_node_widget("Name", "TLK Reference").valueChanged.connect(self.tlk_name_changed)
-        self.init_line_edit("Name", "TLK Text")
-        self.get_node_widget("Name", "TLK Text").setReadOnly(True)
-        self.init_line_edit("Name", "English")
-        self.init_line_edit("Name", "French")
-        self.init_line_edit("Name", "German")
-        self.init_line_edit("Name", "Italian")
-        self.init_line_edit("Name", "Spanish")
-        self.init_line_edit("Name", "Polish")
-        self.init_line_edit("Name", "Korean")
+        self.init_localized_string_nodes("Name")
+        self.init_localized_string_nodes("Description", multiline=True)
 
-        self.init_spin_box("Description", "TLK Reference")
-        self.get_node_widget("Description", "TLK Reference").valueChanged.connect(self.tlk_description_changed)
-        self.init_multiline_edit("Description", "TLK Text")
-        self.get_node_widget("Description", "TLK Text").setReadOnly(True)
-        self.init_multiline_edit("Description", "English")
-        self.init_multiline_edit("Description", "French")
-        self.init_multiline_edit("Description", "German")
-        self.init_multiline_edit("Description", "Italian")
-        self.init_multiline_edit("Description", "Spanish")
-        self.init_multiline_edit("Description", "Polish")
-        self.init_multiline_edit("Description", "Korean")
-
-        self.init_spin_box("Advanced", "Palette ID")
         self.init_spin_box("Advanced", "Upgrade Level")
         self.init_spin_box("Advanced", "Charges")
         self.init_spin_box("Advanced", "Stack Size")
@@ -73,8 +51,6 @@ class ItemEditor(AbstractTreeEditor):
         self.init_spin_box("Advanced", "Texture Variation")
 
         if self.installation is None:
-            self.get_node("Name", "TLK Text").parent().removeChild(self.get_node("Name", "TLK Text"))
-            self.get_node("Description", "TLK Text").parent().removeChild(self.get_node("Description", "TLK Text"))
             self.init_spin_box("Basic", "Base")
         else:
             self.init_combo_box("Basic", "Base", items=Installation.get_base_item_list(self.installation))
@@ -83,20 +59,6 @@ class ItemEditor(AbstractTreeEditor):
     def open_properties_dialog(self):
         # TODO
         print("open properties dialog")
-
-    def tlk_name_changed(self, index):
-        if self.installation is not None:
-            self.get_node_widget("Name", "TLK Text").setText("")
-            if index < self.installation.get_tlk_entry_count():
-                text = self.installation.get_tlk_entry_text(index)
-                self.get_node_widget("Name", "TLK Text").setText(text)
-
-    def tlk_description_changed(self, index):
-        if self.installation is not None:
-            self.get_node_widget("Description", "TLK Text").setPlainText("")
-            if index < self.installation.get_tlk_entry_count():
-                text = self.installation.get_tlk_entry_text(index)
-                self.get_node_widget("Description", "TLK Text").setPlainText(text)
 
     def base_item_changed(self, index):
         try:
@@ -110,4 +72,22 @@ class ItemEditor(AbstractTreeEditor):
             self.model_renderer.objects.append(Object(model_name))
         except Exception as e:
             print("Failed to load door appearance model:", e)
+
+    def load(self, utw):
+        self.set_note_data("Basic", "Script Tag", utw.find_field_data("Tag", default=""))
+        self.set_note_data("Basic", "Template", utw.find_field_data("TemplateResRef", default=""))
+        self.set_note_data("Basic", "Base", utw.find_field_data("BaseItem", default=0))
+        self.set_note_data("Basic", "Cost", utw.find_field_data("Cost", default=0))
+        self.set_note_data("Basic", "Additional Cost", utw.find_field_data("AddCost", default=0))
+        self.set_note_data("Basic", "Plot Item", utw.find_field_data("Plot", default=False))
+
+        self.set_note_data("Advanced", "Upgrade Level", utw.find_field_data("", default=0))
+        self.set_note_data("Advanced", "Charges", utw.find_field_data("Charges", default=0))
+        self.set_note_data("Advanced", "Stack Size", utw.find_field_data("StackSize", default=0))
+        self.set_note_data("Advanced", "Model Variation", utw.find_field_data("ModelVariation", default=0))
+        self.set_note_data("Advanced", "Body Variation", utw.find_field_data("BodyVariation", default=0))
+        self.set_note_data("Advanced", "Texture Variation", utw.find_field_data("TextureVar", default=0))
+
+        self.set_localized_string_nodes("Name", utw.find_field_data("LocalizedName"))
+        self.set_localized_string_nodes("Description", utw.find_field_data("Description"))
 
