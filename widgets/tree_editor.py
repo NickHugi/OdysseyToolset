bc9681ee67e6013cc01d8c3c99b42842a197a11e
cortisol <hugi.nicholas@hotmail.com>
2020-03-11 23:55:33 +0800

    additional functionality

--- widgets/tree_editor.py
+++ widgets/tree_editor.py
@@ -2,7 +2,9 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import QSize
 from PyQt5.QtGui import QBrush, QFontInfo, QFontMetrics
 from PyQt5.QtWidgets import QWidget, QPushButton, QLineEdit, QSpinBox, QCheckBox, QComboBox, QSlider, QTreeWidgetItem, \
-    QPlainTextEdit, QFrame, QSizePolicy, QLabel
+    QPlainTextEdit, QFrame, QSizePolicy, QLabel, QDoubleSpinBox
+
+from pykotor.formats.gff import LocalizedString
 from pykotor.globals import Language
 
 
@@ -39,6 +41,16 @@ class AbstractTreeEditor(QWidget):
 
     def init_spin_box(self, catergory, field, min=0, max=999999, default=0):
         spin_box = QSpinBox()
+        spin_box.setMaximum(max)
+        spin_box.setMinimum(min)
+        spin_box.setFixedHeight(23)
+        spin_box.setValue(default)
+        spin_box.setStyleSheet("background: rgb(0,0,0,0%); border-width: 0px; border-style: none;")
+        self.ui.tree.setItemWidget(self.get_node(catergory, field), 1, spin_box)
+        return spin_box
+
+    def init_double_spin_box(self, catergory, field, min=0, max=999999, default=0):
+        spin_box = QDoubleSpinBox()
         spin_box.setMaximum(max)
         spin_box.setMinimum(min)
         spin_box.setFixedHeight(23)
@@ -88,6 +100,9 @@ class AbstractTreeEditor(QWidget):
     def set_note_data(self, catergory, field, data):
         widget = self.get_node_widget(catergory, field)
 
+        if type(data) is int and data == 4294967295:
+            data = -1
+
         if type(widget) is QLineEdit:
             if type(data) is str:
                 widget.setText(data)
@@ -100,6 +115,54 @@ class AbstractTreeEditor(QWidget):
         if type(widget) is QSpinBox:
             if type(data) is int:
                 widget.setValue(data)
+        if type(widget) is QDoubleSpinBox:
+            if type(data) is float:
+                widget.setValue(data)
+        if type(widget) is QSlider:
+            if type(data) is int:
+                widget.setValue(data)
+        if type(widget) is QComboBox:
+            if type(data) is int:
+                widget.setCurrentIndex(data)
+        if type(widget) is QPlainTextEdit:
+            if type(data) is str:
+                widget.setPlainText(data)
+
+    def get_node_data(self, category, field):
+        widget = self.get_node_widget(category, field)
+
+        if type(widget) is QLineEdit:
+            return widget.text()
+        if type(widget) is QCheckBox:
+            if widget.isChecked(): return 1
+            else: return 0
+        if type(widget) is QSpinBox:
+            return widget.value()
+        if type(widget) is QSlider:
+            return widget.value()
+        if type(widget) is QDoubleSpinBox:
+            return widget.value()
+        if type(widget) is QComboBox:
+            return widget.currentIndex()
+        if type(widget) is QPlainTextEdit:
+            return widget.toPlainText()
+
+    def get_node_localized_string(self, category):
+        localized_string = LocalizedString(0, {})
+        localized_string.string_id = self.get_node_data(category, "TLK Reference")
+        if self.get_node_data(category, "English") != "":
+            localized_string.substrings[Language.English * 2] = self.get_node_data(category, "English")
+        if self.get_node_data(category, "French") != "":
+            localized_string.substrings[Language.French * 2] = self.get_node_data(category, "French")
+        if self.get_node_data(category, "German") != "":
+            localized_string.substrings[Language.German * 2] = self.get_node_data(category, "German")
+        if self.get_node_data(category, "Italian") != "":
+            localized_string.substrings[Language.Italian * 2] = self.get_node_data(category, "Italian")
+        if self.get_node_data(category, "Spanish") != "":
+            localized_string.substrings[Language.Spanish * 2] = self.get_node_data(category, "Spanish")
+        if self.get_node_data(category, "Polish") != "":
+            localized_string.substrings[Language.Polish * 2] = self.get_node_data(category, "Polish")
+        return localized_string
 
     def set_localized_string_nodes(self, category, localized_string):
         if localized_string is not None:
